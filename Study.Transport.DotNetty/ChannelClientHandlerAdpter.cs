12ad0064345194ab39af45d6a07401e416952800
brucehu123 <bruce_hu123@163.com>
2019-03-14 23:07:11 +0800

    dotnetty客户端通道处理类实现

--- Study.Transport.DotNetty/ChannelClientHandlerAdpter.cs
+++ Study.Transport.DotNetty/ChannelClientHandlerAdpter.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using DotNetty.Transport.Channels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Study.Transport.DotNetty
 {
-    class ChannelClientHandlerAdpter
+    public class ChannelClientHandlerAdpter : ChannelHandlerAdapter
     {
+        public override void ChannelActive(IChannelHandlerContext context)
+        {
+            base.ChannelActive(context);
+        }
+
+        public override void ChannelInactive(IChannelHandlerContext context)
+        {
+            base.ChannelInactive(context);
+        }
     }
 }
